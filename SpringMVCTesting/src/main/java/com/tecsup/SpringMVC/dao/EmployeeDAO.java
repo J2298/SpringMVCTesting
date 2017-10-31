c7101bca80fb1e63b348909326e700b70a50cee3
J2298 <job.cantaro@tecsup.edu.pe>
2017-10-31 10:24:25 -0500

    Define Commit

--- SpringMVCTesting/src/main/java/com/tecsup/SpringMVC/dao/EmployeeDAO.java
+++ SpringMVCTesting/src/main/java/com/tecsup/SpringMVC/dao/EmployeeDAO.java
@@ -1,5 +1,7 @@
 package com.tecsup.SpringMVC.dao;
 
+import java.util.List;
+
 import com.tecsup.SpringMVC.exception.DAOException;
 import com.tecsup.SpringMVC.exception.EmptyResultException;
 import com.tecsup.SpringMVC.model.Employee;
@@ -8,4 +10,17 @@ public interface EmployeeDAO {
 
 	Employee findEmployee(int id) throws DAOException, EmptyResultException;
 
+	void create(String login, String password, String lastname, String firstname, int salary, int dptId) throws DAOException;
+
+	void delete(String login) throws DAOException;
+
+	void update(String login, String password, String lastname, String firstname, int salary, int dptId) throws DAOException;
+
+	Employee findEmployeeByLogin(String login) throws DAOException, EmptyResultException;
+
+	List<Employee> findAllEmployees() throws DAOException, EmptyResultException;
+
+	List<Employee> findEmployeesByName(String name) throws DAOException, EmptyResultException;
+
 }
+
